ed6b017dd2b9f158e82b93ae56b1cdd6f3d810bf
henriits <henritsarents@hotmail.com>
2024-09-26 20:31:52 +0300

    feat: display online - offline users count

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -2,18 +2,23 @@ import User from '@/components/User/User'
 import './UserList.css'
 
 function UserList({ users }) {
+  const onlineUsers = users.filter(user => user.connected)
+  const offlineUsers = users.filter(user => !user.connected)
+
   return (
     <div className="user-list">
       <div className="online-users">
-        <p className="user-status">ONLINE</p>
-        {users.length > 0 &&
-          users.filter(user => user.connected).map(user => <User key={user.userId} user={user} />)}
+        <p className="user-status">ONLINE -- {onlineUsers.length}</p>
+        {onlineUsers.map(user => (
+          <User key={user.userId} user={user} />
+        ))}
       </div>
 
       <div className="offline-users">
-        <p className="user-status">OFFLINE</p>
-        {users.length > 0 &&
-          users.filter(user => !user.connected).map(user => <User key={user.userId} user={user} />)}
+        <p className="user-status">OFFLINE -- {offlineUsers.length}</p>
+        {offlineUsers.map(user => (
+          <User key={user.userId} user={user} />
+        ))}
       </div>
     </div>
   )
